0dde3edd5b32113a9d80396a8754281bfcece4c2
Frostdip <jun.yu@edu.immaginazioneelavoro.it>
2025-03-20 11:06:22 +0100

    Sito/Pt1

---
+++ script.js
@@ -0,0 +1,14 @@
+let a = 0; 
+
+function compariMenu() {
+    if (a==0)   {
+        document.getElementById("menu").style.display = "initial";
+        a = 1;
+    }
+    else if (a==1) {
+        document.getElementById("menu").style.display = "none";
+        a = 0;
+    }
+   
+}
+
