0efa2a631f7221ee747f9b24c529db62a5b9895e
Frostdip <jun.yu@edu.immaginazioneelavoro.it>
2025-06-10 11:41:47 +0200

    FInal

--- script.js
+++ script.js
@@ -12,33 +12,3 @@ function compariMenu() {
     }
    
 }
-
-function cambiaTesto() {
-    document.getElementById("Testo1").innerText = "La mia passione principale è quella di giocare a videogiochi\nche richiedono strategia";
-}
-
-function ritornaTesto() {
-     document.getElementById("Testo1").innerText = "";
-}
-
-function cambiaTesto2() {
-    document.getElementById("Testo2").innerText = "Mi piace leggere Manga/Manhwa per passare il tempo";
-}
-
-function ritornaTesto2() {
-     document.getElementById("Testo2").innerText = "";
-}
-function cambiaTesto3() {
-    document.getElementById("Testo3").innerText = "Guardare tornei di giochi";
-}
-
-function ritornaTesto3() {
-     document.getElementById("Testo3").innerText = "";
-}
-function cambiaTesto4() {
-    document.getElementById("Testo4").innerText = "Mi piace giocare a scacchi";
-}
-
-function ritornaTesto4() {
-     document.getElementById("Testo4").innerText = "";
-}
